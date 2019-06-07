69b06536c3602c79260d65db81349e01f8d96e80
Hady Ahmed <h.a7med2017@gmail.com>
2019-06-08 00:16:03 +0200

    ticket qr code v1.0.0

---
+++ app/src/main/java/com/hadi/trainticketing/passenger/view/features/TicketQrCodeActivity.java
@@ -0,0 +1,49 @@
+package com.hadi.trainticketing.passenger.view.features;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.zxing.BarcodeFormat;
+import com.hadi.trainticketing.R;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+public class TicketQrCodeActivity extends AppCompatActivity {
+
+    public static final String RESERVATION_ID_EXTRA = "reservation_id";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_code);
+
+        Toolbar toolbar = findViewById(R.id.qrCodeToolbar);
+        toolbar.setNavigationOnClickListener(v -> {
+            startActivity(new Intent(TicketQrCodeActivity.this, TicketActivity.class));
+            finish();
+        });
+
+        String reservationId = getIntent().getStringExtra(RESERVATION_ID_EXTRA);
+
+        try {
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(reservationId, BarcodeFormat.QR_CODE, 700, 700);
+            ImageView imageViewQrCode = findViewById(R.id.ticketQrCodeIV);
+            imageViewQrCode.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            Toast.makeText(this, "couldn't load ticket qr code", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(TicketQrCodeActivity.this, TicketActivity.class));
+        finish();
+    }
+}
